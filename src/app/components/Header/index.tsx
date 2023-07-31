b68aa4060df40e0dfc4e7dc90b3e4eee1b2260af
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-07-31 17:24:33 -0300

    Added the Auth-Modal to the app. Added chakra-icons support.

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import styles from './styles.module.css';
 import RightSideContent from './RightSideContent';
 import LeftSideContent from './LeftSideContent';
+import AuthModal from '../Modal/Auth/AuthModal';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
+      <AuthModal />
       <div className={styles.content_wrapper}>
         <LeftSideContent />
         <RightSideContent />
