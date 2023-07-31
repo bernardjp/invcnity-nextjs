8b7e48b0d127188fd10bd107f24b451636f16968
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-07-31 14:13:11 -0300

    Removed 'inter' next/font from body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,6 @@
 import './global.css';
 import { StyleProviders } from '@/style/StyleProvider';
-import { Inter } from 'next/font/google';
 import Header from './components/Header';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'IN/VCNITY',
@@ -17,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <StyleProviders>
           <Header />
           {children}
