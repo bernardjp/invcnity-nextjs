b8ae787ba3fc67904f15362a2b86dc9687596956
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-07-31 17:24:33 -0300

    Added the Auth-Modal to the app. Added chakra-icons support.

--- src/app/components/Header/RightSideContent/index.tsx
+++ src/app/components/Header/RightSideContent/index.tsx
@@ -1,17 +1,17 @@
 'use client';
 import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Button } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useAuthModal } from '@/app/hooks/useAuthModal';
 
 const RightSideContent: React.FC = () => {
+  const { openModal } = useAuthModal();
+
   return (
-    <Flex>
-      <Flex alignItems="center">
-        <Link href="listas">Listas</Link>
-        <Link href="propiedades">Propiedades</Link>
-      </Flex>
-      {/* Authentication button: Sign Up | Log In */}
-      <button>Sign Up</button>
+    <Flex alignItems="center" gap={4}>
+      <Link href="listas">Listas</Link>
+      <Link href="propiedades">Propiedades</Link>
+      <Button onClick={() => openModal()}>Sign Up</Button>
     </Flex>
   );
 };
