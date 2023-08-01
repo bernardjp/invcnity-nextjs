c2cfa27ba21f7f0039a505036f15d2ce1edac0c0
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-01 17:52:23 -0300

    added styled inputs components to handle text and password type inputs.

---
+++ src/app/components/Modal/Auth/StyledInput.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Input } from '@chakra-ui/react';
+
+type StyledInputProps = {
+  type: 'text' | 'password';
+  name: 'email' | 'username' | 'password' | 'confirmPassword';
+  placeholder: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const StyledInput: React.FC<StyledInputProps> = (props: StyledInputProps) => {
+  const { type, name, placeholder, onChange } = props;
+
+  return (
+    <Input
+      pr={type === 'password' ? '3rem' : ''}
+      type={type}
+      name={name}
+      backgroundColor="gray.50"
+      placeholder={placeholder}
+      _placeholder={{ color: 'gray.700' }}
+      _hover={{
+        border: '1px solid',
+        borderColor: 'blue.400',
+      }}
+      _focus={{
+        bg: 'white',
+        outline: 'none',
+        border: '1px solid',
+        borderColor: 'blue.400',
+      }}
+      onChange={onChange}
+    />
+  );
+};
+export default StyledInput;
