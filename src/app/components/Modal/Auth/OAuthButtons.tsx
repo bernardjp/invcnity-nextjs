db78bddf23f2ba344cca8cf6a35ca5035e12b7e3
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-03 18:12:02 -0300

    Added OAuth Signup functionality with Firebase/Google.

--- src/app/components/Modal/Auth/OAuthButtons.tsx
+++ src/app/components/Modal/Auth/OAuthButtons.tsx
@@ -1,14 +1,25 @@
-import { Button, Image, Stack } from '@chakra-ui/react';
 import React from 'react';
+import { Button, Image, Stack, Text } from '@chakra-ui/react';
+import { auth } from '@/firebase/clientApp';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { FIREBASE_ERRORS } from '@/firebase/errors';
 
 function OAuthButtons(): React.ReactElement {
+  const [signInWithGoogle, user, loading, userError] =
+    useSignInWithGoogle(auth);
+
+  const googleAuthHandler = () => {
+    signInWithGoogle();
+  };
+
   return (
-    <Stack mb={5} gap={1}>
+    <Stack gap={1}>
       <Button
         borderColor="gray.300"
         variant="outline"
         borderRadius={45}
-        onClick={() => {}}
+        isLoading={loading}
+        onClick={() => googleAuthHandler()}
       >
         <Image
           borderRadius="full"
@@ -19,6 +30,11 @@ function OAuthButtons(): React.ReactElement {
         />
         Continue with Google
       </Button>
+      {userError && (
+        <Text textAlign="center" fontSize={14} color="red.400">
+          {FIREBASE_ERRORS[userError.message as keyof typeof FIREBASE_ERRORS]}
+        </Text>
+      )}
     </Stack>
   );
 }
