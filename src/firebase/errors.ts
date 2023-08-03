06294529cbcc3ea8450f244225455b32f855871f
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-03 18:07:40 -0300

    Added Firebase error-message mapping.

---
+++ src/firebase/errors.ts
@@ -0,0 +1,6 @@
+export const FIREBASE_ERRORS = {
+  'Firebase: Error (auth/email-already-in-use).':
+    'Error: Email already in use.',
+  'Firebase: Error (auth/user-not-found).': 'Invalid email or password',
+  'Firebase: Error (auth/wrong-password).': 'Invalid email or password',
+};
