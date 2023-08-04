404f1fa04e0db3014ff0f765a55f21807dda4b34
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-04 13:03:01 -0300

    Wrapped modal's open/close behavior with useCallback to avoid re-rendering loops.

--- src/app/hooks/useAuthModal.ts
+++ src/app/hooks/useAuthModal.ts
@@ -1,11 +1,20 @@
 import { useRecoilState } from 'recoil';
 import { authModalAtom, ViewType } from '@/recoil/AuthModalAtom';
+import { useCallback } from 'react';
 
 export const useAuthModal = () => {
   const [modalState, setModalState] = useRecoilState(authModalAtom);
 
-  const closeModal = () => setModalState((prev) => ({ ...prev, open: false }));
-  const openModal = (view: ViewType) => setModalState({ view, open: true });
+  const closeModal = useCallback(
+    () => setModalState((prev) => ({ ...prev, open: false })),
+    [setModalState]
+  );
+
+  const openModal = useCallback(
+    (view: ViewType) => setModalState({ view, open: true }),
+    [setModalState]
+  );
+
   const changeView = (view: ViewType) =>
     setModalState((prev) => ({ ...prev, view }));
 
