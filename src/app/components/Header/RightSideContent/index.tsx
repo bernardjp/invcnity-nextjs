758bcce46dcc1766e8bf6dba4b2840b7eaad6598
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-07 12:04:27 -0300

    Added Logout button and functionality and authenticated conditional rendering to Header's navigation.

--- src/app/components/Header/RightSideContent/index.tsx
+++ src/app/components/Header/RightSideContent/index.tsx
@@ -3,16 +3,35 @@ import React from 'react';
 import { Flex, Button } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useAuthModal } from '@/app/hooks/useAuthModal';
+import { useSignOut } from 'react-firebase-hooks/auth';
+import { User } from 'firebase/auth';
+import { auth } from '@/firebase/clientApp';
 
-const RightSideContent: React.FC = () => {
+type PropsType = {
+  user: User | null | undefined;
+};
+
+const RightSideContent: React.FC<PropsType> = (props: PropsType) => {
+  const { user } = props;
   const { openModal } = useAuthModal();
+  const [signOut] = useSignOut(auth);
+
+  const logoutHandler = () => signOut();
 
   return (
     <Flex alignItems="center" gap={4}>
-      <Link href="listas">Listas</Link>
-      <Link href="propiedades">Propiedades</Link>
-      <Button onClick={() => openModal('login')}>Login</Button>
-      <Button onClick={() => openModal('signup')}>Sign Up</Button>
+      {user ? (
+        <>
+          <Link href="listas">Listas</Link>
+          <Link href="propiedades">Propiedades</Link>
+          <Button onClick={logoutHandler}>Logout</Button>
+        </>
+      ) : (
+        <>
+          <Button onClick={() => openModal('login')}>Login</Button>
+          <Button onClick={() => openModal('signup')}>Sign Up</Button>
+        </>
+      )}
     </Flex>
   );
 };
