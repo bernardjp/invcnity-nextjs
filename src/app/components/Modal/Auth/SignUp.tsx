084b4f36a7209c174df1d73a599c831b13e9ea62
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-10 13:59:12 -0300

    Added username uniqueness verification to Sign-up. Added a Firestore's UserDocument creation to the basic and OAuth signup process.

--- src/app/components/Modal/Auth/SignUp.tsx
+++ src/app/components/Modal/Auth/SignUp.tsx
@@ -3,18 +3,21 @@ import { Button, Stack, Text } from '@chakra-ui/react';
 import StyledInput from './StyledInput';
 import PasswordInput from './PasswordInput';
 import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
-import { auth } from '@/firebase/clientApp';
 import { SignUpValidation, validateSignUpForm } from './utils/validation';
+import { auth } from '@/firebase/clientApp';
 import { FIREBASE_ERRORS } from '@/firebase/errors';
+import { createUserDocument, verifyUniqueUsername } from '@/firebase/authUtils';
 import { updateProfile } from 'firebase/auth';
 
+const FORM_DEFAULT_VALUES = {
+  username: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+};
+
 function SignUp(): React.ReactElement {
-  const [signupForm, setSignupForm] = useState({
-    username: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-  });
+  const [signupForm, setSignupForm] = useState(FORM_DEFAULT_VALUES);
   const [createUserWithEmailAndPassword, user, loading, userError] =
     useCreateUserWithEmailAndPassword(auth);
   const [formError, setFormError] = useState<SignUpValidation | null>(null);
@@ -31,19 +34,43 @@ function SignUp(): React.ReactElement {
     e.preventDefault();
     setFormError(null); // Reset the validation errors.
 
+    // Extracs the data from the form.
     const { username, email, password, confirmPassword } = signupForm;
+
+    // Validate the format of the data.
     const formValidation = validateSignUpForm(
       username,
       email,
       password,
       confirmPassword
     );
-
-    if (formValidation.isValidated) {
-      await createUserWithEmailAndPassword(email, password);
-      await updateProfile(auth.currentUser!, { displayName: username });
-    } else {
+    if (!formValidation.isValidated) {
       setFormError(formValidation);
+      return;
+    }
+
+    // Verifies the uniqueness of the username in the DB.
+    const isUsernameUnique = await verifyUniqueUsername(username);
+    if (!isUsernameUnique) {
+      setFormError({
+        ...formValidation,
+        username: {
+          errorMessage: `Username ${username} already exist. Try another one.`,
+          isValidated: false,
+        },
+      });
+      return;
+    }
+
+    // Registers the user in the AuthDB and creates a new user in the DocumentsDB.
+    try {
+      const newUser = await createUserWithEmailAndPassword(email, password);
+      await updateProfile(auth.currentUser!, { displayName: username });
+
+      if (newUser) await createUserDocument(newUser);
+      setFormError(null);
+    } catch (error) {
+      console.log(error);
     }
   };
 
