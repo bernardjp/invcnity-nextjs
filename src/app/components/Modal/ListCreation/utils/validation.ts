dbfb7446aab0eaa704d322e683bef363821adff0
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-17 19:34:46 -0300

    Added validation to the List-Creation form.

---
+++ src/app/components/Modal/ListCreation/utils/validation.ts
@@ -0,0 +1,62 @@
+type InputValidation = {
+  errorMessage: string;
+  isValidated: boolean;
+};
+
+export type ListType = 'apartment' | 'house' | 'countryside' | 'vacation';
+
+export type ListFormValidation = {
+  listName: InputValidation;
+  type: InputValidation;
+  isValidated: boolean;
+};
+
+export type ListInfoType = {
+  listName: string;
+  type: ListType;
+};
+
+function validateListName(listName: string): InputValidation {
+  const LISTNAME_REGEX = /^[a-zA-ZÀ-ÿ0-9\\u00f1\\u00d1\s\-]{3,32}$/gm;
+  const isValidated = LISTNAME_REGEX.test(listName);
+
+  if (listName.trim().length === 0) {
+    return {
+      errorMessage: 'VCNITY name is required.',
+      isValidated: false,
+    };
+  }
+
+  return {
+    errorMessage: isValidated
+      ? ''
+      : 'The VCNITY name must range between 3 and 32 characters long. Only alphanumerical characters and scores [-] are allowed',
+    isValidated,
+  };
+}
+
+function validateType(type: string): InputValidation {
+  const TYPES = ['apartment', 'house', 'countryside', 'vacation'];
+  const isValidated = TYPES.includes(type.toLowerCase());
+
+  return {
+    errorMessage: isValidated ? '' : 'Pick a valid type of VCNITY.',
+    isValidated,
+  };
+}
+
+export function validateListForm(
+  listName: string,
+  type: string
+): ListFormValidation {
+  const listNameValidation = validateListName(listName);
+  const typeValidation = validateType(type);
+  const isValidated =
+    listNameValidation.isValidated && typeValidation.isValidated;
+
+  return {
+    listName: listNameValidation,
+    type: typeValidation,
+    isValidated,
+  };
+}
