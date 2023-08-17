9a858bd48d424e9c7b50189ff805d731acea3a70
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-17 19:15:33 -0300

    Added Footer to Root Layout. Adjusted List-Page layout height.

--- src/app/listas/layout.tsx
+++ src/app/listas/layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './styles.module.css';
 
 type Props = {
   children: React.ReactNode;
 };
 
 function ListsPageLayout(props: Props) {
-  return <div>{props.children}</div>;
+  return <div className={styles.page}>{props.children}</div>;
 }
 
 export default ListsPageLayout;
