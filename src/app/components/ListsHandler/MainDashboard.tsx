590f14ed7d40750e9c3e64380887c2ba5ae346f0
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-22 18:20:50 -0300

    Added Firestores listeners to the Main-Dashboard component to help render the list dinamically.

--- src/app/components/ListsHandler/MainDashboard.tsx
+++ src/app/components/ListsHandler/MainDashboard.tsx
@@ -2,28 +2,32 @@ import React from 'react';
 import Link from 'next/link';
 import { User } from 'firebase/auth';
 import { useCreateResourceModal } from '@/app/hooks/useCreateResourceModal';
+import { ListInfoType } from '../Modal/ListCreation/utils/validation';
 
 type ListDashboardProps = {
   user: User | null | undefined;
+  listSnippets: ListInfoType[];
 };
 
 function ListDashboard(props: ListDashboardProps): React.ReactElement {
-  const { user } = props;
+  const { user, listSnippets } = props;
   const { openModal } = useCreateResourceModal('list');
 
   return (
     <section>
       <h1>Welcome {user?.displayName}</h1>
       <div style={{ display: 'flex', gap: '1rem' }}>
-        <Link href="/listas/123" style={{ padding: '1rem' }}>
-          List 123
-        </Link>
-        <Link href="/listas/456" style={{ padding: '1rem' }}>
-          List 456
-        </Link>
-        <Link href="/listas/789" style={{ padding: '1rem' }}>
-          List 789
-        </Link>
+        {listSnippets!.map((list) => {
+          return (
+            <Link
+              key={list.listName}
+              href={`/listas/${list.id}`}
+              style={{ padding: '1rem' }}
+            >
+              {list.listName}
+            </Link>
+          );
+        })}
         <button style={{ padding: '1rem' }} onClick={() => openModal()}>
           Add list
         </button>
--- src/app/components/ListsHandler/index.tsx
+++ src/app/components/ListsHandler/index.tsx
@@ -1,16 +1,30 @@
 'use client';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth } from '@/firebase/clientApp';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import SignInHero from './SignInHero';
 import MainDashboard from './MainDashboard';
+import { ListInfoType } from '../Modal/ListCreation/utils/validation';
+import { getListSnippets } from '@/firebase/firestoreUtils';
 
 function ListHandlerPage(): React.ReactElement {
   const [user, loading, error] = useAuthState(auth);
+  const [listData, setListData] = useState<ListInfoType[]>([]);
+
+  useEffect(() => {
+    if (!user) return;
+    // Setups a listener that catches changes to the store in real time.
+    // This way the list updates dinamically.
+    getListSnippets(user.uid, setListData);
+  }, [user]);
 
   return (
     <>
-      {user && !loading ? <MainDashboard user={user} /> : <SignInHero />}
+      {user && !loading ? (
+        <MainDashboard user={user} listSnippets={listData} />
+      ) : (
+        <SignInHero />
+      )}
       {error && <div>{error.message}</div>}
     </>
   );
