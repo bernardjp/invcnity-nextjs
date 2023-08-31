7d359411c85a0f46a2248c2cc183f02f473162c9
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-08-31 17:00:47 -0300

    Added Global theme styles addressing: Brand colors, fonts, bg colors, and Components variants.

--- src/style/StyleProvider.tsx
+++ src/style/StyleProvider.tsx
@@ -1,12 +1,12 @@
 'use client';
-
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './theme';
 
 export function StyleProviders({ children }: { children: React.ReactNode }) {
   return (
     <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
     </CacheProvider>
   );
 }
