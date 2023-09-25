d3812c0ce82aaf3b9e8f6577fce044cb58e15d7e
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-09-25 17:04:31 -0300

    Refactored FavoriteIcon to make use of CardIcon component. Implemented this changes on the BasCard component.

--- src/app/components/Card/CardFavoriteIcon.tsx
+++ src/app/components/Card/CardFavoriteIcon.tsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
-import { Flex, Image, useStyleConfig } from '@chakra-ui/react';
+import { Image } from '@chakra-ui/react';
 import { ThemeVariant } from '@/style/componentsStyleConfig';
+import CardIcon from './CardIcon';
 
 function CardFavoriteIcon(props: {
   variant: ThemeVariant;
   isFavorite?: boolean;
 }): React.ReactElement {
   const { variant, isFavorite } = props;
-  const iconStyles = useStyleConfig('CardIcon', { variant });
 
   const [favoriteVal, setFavorite] = useState(isFavorite);
 
@@ -17,34 +17,13 @@ function CardFavoriteIcon(props: {
   };
 
   return (
-    <Flex
-      border="2px solid"
-      borderRadius="full"
-      height="41px"
-      justifyContent="center"
-      opacity={0.6}
-      p="3px"
-      position="absolute"
-      left={2}
-      top={2}
-      transition="0.15s"
-      width="41px"
-      zIndex={5} // The Icon MUST be in front of the card to be clickable
-      _hover={{
-        opacity: '1',
-        border: '2px solid',
-      }}
-      __css={iconStyles}
-      onClick={onClickHandler}
-    >
+    <CardIcon variant={variant} onClickHandler={onClickHandler}>
       <Image
         src={`/icons/fav-${favoriteVal ? 'on' : 'off'}.svg`}
         alt=""
-        width="90%"
-        pl="3px"
-        pt="3px"
+        width="80%"
       />
-    </Flex>
+    </CardIcon>
   );
 }
 
