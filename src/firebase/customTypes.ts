d33b52eacd120c043f8a6e05ab7911af39b70c33
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-09-26 15:01:17 -0300

    Moved types related to data/validation from validation utils to firebase customTypes.

--- src/firebase/customTypes.ts
+++ src/firebase/customTypes.ts
@@ -1,5 +1,14 @@
 import { UserInfo } from 'firebase/auth';
 
+export type ListType = 'apartment' | 'house' | 'countryside' | 'vacation';
+export type RoleType = 'owner' | 'editor' | 'reader';
+export type ListInfoType = {
+  id: string;
+  listName: string;
+  type: ListType;
+  roles: { [x: string]: RoleType };
+};
+
 export type UserDoc = {
   uid: string;
   username: string | null;
@@ -13,5 +22,14 @@ export type EstateListDoc = {
 };
 
 export type EstateDoc = {
-  /* *** */
+  uid: string;
+  estateName: string;
+  location: string;
+  price: string;
+  publicationURL: string;
+  locationURL: string;
+  type: ListType;
+  isVisited?: boolean;
+  isFavorite?: boolean;
+  roles: { [x: string]: RoleType };
 };
