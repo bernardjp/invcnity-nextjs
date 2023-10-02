dd2c044ae3b844aa8119852a1ff2aa5d0e9be7ca
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-10-02 13:43:00 -0300

    Extrated and centralized all the Firestore's interaction logic to firestore utils.

--- src/firebase/firestoreUtils.ts
+++ src/firebase/firestoreUtils.ts
@@ -1,6 +1,11 @@
-import { collection, onSnapshot } from 'firebase/firestore';
+import {
+  collection,
+  doc,
+  onSnapshot,
+  runTransaction,
+} from 'firebase/firestore';
 import { firestore } from './clientApp';
-import { ListInfoType } from '@/app/components/Modal/ListCreation/utils/validation';
+import { ListInfoType, EstateFormInfo } from './customTypes';
 
 function getListSnippets(
   userID: string,
@@ -19,4 +24,34 @@ function getListSnippets(
   );
 }
 
-export { getListSnippets };
+async function createEstate(estateData: EstateFormInfo) {
+  await runTransaction(firestore, async (transaction) => {
+    const estateDocRef = doc(collection(firestore, 'estates'));
+    transaction.set(estateDocRef, estateData);
+
+    const listDocRef = doc(
+      firestore,
+      `estate_lists/${estateData.listID}/estateSnippets`,
+      estateDocRef.id
+    );
+    transaction.set(listDocRef, estateData);
+  });
+}
+
+function deleteList(listID: string, userID: string) {
+  /* Delete process:
+      1- Delete the EstateSnippets subcollection associated with the List. Alternativatly, delete all the Estate documents associated with the List. (All the Estates must have a reference to the Parent List).
+      2- Delete the List document itself.
+      3- Delete the ListSnippet in each User associated with the List.
+   */
+  console.log('Deleting LIST ID:', listID, 'from USER ID:', userID);
+}
+
+function deleteEstate(estateID: string, listID: string) {
+  /* Delete process:
+      1- Delete the EstateSnippet document from the associated List.
+      2- Delete the Estate document itself.
+   */
+}
+
+export { getListSnippets, createEstate, deleteEstate, deleteList };
