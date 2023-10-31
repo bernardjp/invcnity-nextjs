239b921c8e03811bcbf616fd1ec4232e632d2bf2
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-10-31 18:50:23 -0300

    Added 'type' and 'onClickHandler' as props arguments. Adjusted the component styling based on its type.

--- src/app/components/Modal/StyledSubmitButton.tsx
+++ src/app/components/Modal/StyledSubmitButton.tsx
@@ -3,22 +3,25 @@ import React from 'react';
 
 type Props = {
   loading: boolean;
+  type: 'submit' | 'reset';
   text: string;
+  onClickHandler?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
 function StyledSubmitButton(props: Props) {
-  const { loading, text } = props;
+  const { loading, text, type, onClickHandler } = props;
   return (
     <Button
-      backgroundColor="#49ad87"
-      border="1px solid white"
+      backgroundColor={type === 'reset' ? 'white' : '#49ad87'}
+      border={`1px solid ${type === 'reset' ? '#49ad87' : 'white'}`}
       borderRadius={50}
       boxShadow="0px 4px 15px -5px #49ad87"
-      color="white"
+      color={type === 'reset' ? '#49ad87' : 'white'}
       mt={4}
-      type="submit"
+      type={type}
       width="100%"
       isLoading={loading}
+      onClick={onClickHandler}
       _hover={{
         backgroundColor: 'brand.darkTeal',
       }}
