d17cdf545dc33fa19f0ed8353b2a793ce204f66e
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-11-28 15:26:48 -0300

    Adjusted the Input Label margin on small resolutions.

--- src/app/components/Modal/BaseLabeledInput.tsx
+++ src/app/components/Modal/BaseLabeledInput.tsx
@@ -9,12 +9,8 @@ type Props = {
 function BaseLabeledInput(props: Props) {
   const { label, children } = props;
   return (
-    <VStack mb={2} gap="0">
-      <Text
-        w="100%"
-        color="gray.500"
-        textAlign={{ base: 'center', sm: 'left' }}
-      >
+    <VStack mb={2} gap="0" w="100%">
+      <Text w="100%" color="gray.500" textAlign="left">
         {label}
       </Text>
       <Box w="100%" px="1rem">
