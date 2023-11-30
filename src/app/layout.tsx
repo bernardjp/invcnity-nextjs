c595fab2da5027ed5b2cc27d3dc6d31a7220f263
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-11-30 10:57:23 -0300

    Removed the metadata export from the layout file.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,11 +5,6 @@ import { StyleProviders } from '@/style/StyleProvider';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
-export const metadata = {
-  title: 'IN/VCNITY',
-  description: 'Real Estate CRUD',
-};
-
 export default function RootLayout({
   children,
 }: {
