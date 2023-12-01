a5e9a42efa145501b318c655187b9b5262bff5e3
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-12-01 12:58:16 -0300

    Fixed the background color of the Card's Icons.

--- src/app/components/Card/CardIcon.tsx
+++ src/app/components/Card/CardIcon.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Flex, useStyleConfig } from '@chakra-ui/react';
+import { Box, Flex, useStyleConfig } from '@chakra-ui/react';
 import { ThemeVariant } from '@/style/componentsStyleConfig';
 import CardTooltip from './CardTooltip';
 
@@ -16,28 +16,30 @@ function CardIcon(props: Props): React.ReactElement {
 
   return (
     <CardTooltip label={label} variant={variant}>
-      <Flex
-        border="2px solid"
-        borderRadius="full"
-        boxShadow="0px 5px 10px -5px black"
-        display="flex"
-        height="41px"
-        justifyContent="center"
-        opacity={0.6}
-        p="3px"
-        transition="0.15s"
-        width="41px"
-        zIndex={5} // The Icon MUST be in front of the card to be clickable
-        _hover={{
-          border: '2px solid',
-          cursor: 'pointer',
-          opacity: '1',
-        }}
-        __css={iconStyles}
-        onClick={onClickHandler}
-      >
-        {children}
-      </Flex>
+      <Box bg="white" borderRadius="full">
+        <Flex
+          border="2px solid"
+          borderRadius="full"
+          boxShadow="0px 5px 10px -5px black"
+          display="flex"
+          height="41px"
+          justifyContent="center"
+          opacity={0.6}
+          p="3px"
+          transition="0.15s"
+          width="41px"
+          zIndex={5} // The Icon MUST be in front of the card to be clickable
+          _hover={{
+            border: '2px solid',
+            cursor: 'pointer',
+            opacity: '1',
+          }}
+          __css={iconStyles}
+          onClick={onClickHandler}
+        >
+          {children}
+        </Flex>
+      </Box>
     </CardTooltip>
   );
 }
