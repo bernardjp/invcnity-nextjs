e2a6a9b469ad5106060bf14a57bd3e780b2b7037
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-12-19 11:25:08 -0300

    Refactored useGetUserData: wrapped the data fetching on a single Promise; added custom error message.

--- src/app/hooks/useGetUserData.ts
+++ src/app/hooks/useGetUserData.ts
@@ -10,33 +10,39 @@ import {
 } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
+type UserData = {
+  profile: DocumentSnapshot<DocumentData> | null;
+  snippets: QuerySnapshot<DocumentData> | null;
+};
+
 export function useGetUserData(userID: string) {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState();
-  const [user, setUser] = useState<DocumentSnapshot<DocumentData> | null>(null);
-  const [listSnippets, setListSnippets] =
-    useState<QuerySnapshot<DocumentData> | null>(null);
+  const [userData, setUserData] = useState<UserData>({
+    profile: null,
+    snippets: null,
+  });
 
   useEffect(() => {
-    try {
-      setLoading(true);
-      getDoc(doc(firestore, 'users', userID)).then((data) => setUser(data));
-      getDocs(collection(firestore, `users/${userID}/listSnippets`)).then(
-        (data) => setListSnippets(data)
-      );
-    } catch (error) {
-      setError(error as any);
-    } finally {
-      setLoading(false);
-    }
+    Promise.all([
+      getDoc(doc(firestore, 'users', userID)),
+      getDocs(collection(firestore, `users/${userID}/listSnippets`)),
+    ])
+      .then(([profile, snippets]) => {
+        setUserData({ profile, snippets });
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error as any);
+      });
   }, [userID]);
+
+  useEffect(() => {
+    if (error) throw new Error('Error al cargar la pagina');
+  }, [error]);
 
   return {
     loading,
-    error,
-    data: {
-      user,
-      listSnippets,
-    },
+    data: userData,
   };
 }
