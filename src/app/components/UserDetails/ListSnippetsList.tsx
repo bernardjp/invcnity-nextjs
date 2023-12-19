900eb4bd65dd8d12c4ec13b69c43da5ed36cd1ad
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-12-19 11:09:59 -0300

    Added a Default ListSnippet to the User page that redirects to List Page.

--- src/app/components/UserDetails/ListSnippetsList.tsx
+++ src/app/components/UserDetails/ListSnippetsList.tsx
@@ -2,19 +2,63 @@ import { EstateListDoc } from '@/firebase/customTypes';
 import { Flex } from '@chakra-ui/react';
 import React from 'react';
 import ListSnippet from './ListSnippet';
+import Link from 'next/link';
+import { AddIcon } from '@chakra-ui/icons';
+
+function DefaultSnippet() {
+  return (
+    <Link href="/listas">
+      <Flex
+        alignItems="center"
+        bg="brand.lightRed"
+        border="3px dotted"
+        borderColor="brand.red"
+        borderRadius="24px"
+        boxShadow="0px 5px 15px -7px rgba(0,0,0,0.5)"
+        color="brand.red"
+        fontSize="1.1rem"
+        fontWeight="bold"
+        gap="1rem"
+        p="5px 5px 5px 18px"
+        position="relative"
+        transition="0.2s"
+        _hover={{
+          transform: 'translate(0, -3px)',
+        }}
+      >
+        Start Creating VCNITIES!
+        <Flex
+          alignItems="center"
+          bg="white"
+          border="2px solid"
+          borderColor="brand.red"
+          borderRadius="full"
+          h="fit-content"
+          opacity={0.7}
+          p="10px"
+        >
+          <AddIcon boxSize={4} color="brand.red" />
+        </Flex>
+      </Flex>
+    </Link>
+  );
+}
 
 export default function ListSnippetsList(props: {
-  listData?: EstateListDoc[];
+  listData: EstateListDoc[];
   userID: string;
 }) {
   const { listData, userID } = props;
 
   return (
     <Flex w="100%" gap="2rem">
-      {listData &&
+      {listData.length > 0 ? (
         listData.map((list) => (
           <ListSnippet key={list.id} listData={list} userID={userID} />
-        ))}
+        ))
+      ) : (
+        <DefaultSnippet />
+      )}
     </Flex>
   );
 }
