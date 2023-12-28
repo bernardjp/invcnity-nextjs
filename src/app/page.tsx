926ff643d3c841931d875856c0840f9f697ec85d
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2023-12-28 18:55:46 -0300

    Added basic structure and styling to the landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import { Metadata } from 'next';
+import Image from 'next/image';
+import styles from './style.module.css';
+import ActionButton from './components/LandingPage/ActionButton';
 
 export const metadata: Metadata = {
   title: 'IN/VCNITY',
@@ -7,9 +10,34 @@ export const metadata: Metadata = {
 
 function Home() {
   return (
-    <div>
-      <h1>IN/VCNITY</h1>
-    </div>
+    <>
+      <article className={`${styles.hero} ${styles.wrapper}`}>
+        <div className={styles.image_container}>
+          <Image
+            src="/images/select-family.svg"
+            alt="couple searching for their dream place"
+            width={720}
+            height={10}
+          />
+        </div>
+
+        <div className={styles.text_container}>
+          <div className={styles.title_container}>
+            <h1 className={styles.title}>IN/VCNITY</h1>
+            <span className={styles.title_underline}></span>
+          </div>
+          <p className={styles.text}>The place to find your place</p>
+          <ActionButton />
+        </div>
+      </article>
+      <article className={`${styles.about} ${styles.wrapper}`}>
+        <div className={styles.content_container}>What is IN/VCNITY?</div>
+      </article>
+      {/* <article className={styles.contact_wrapper}> */}
+      <article className={`${styles.contact} ${styles.wrapper}`}>
+        <div className={styles.content_container}>What is IN/VCNITY?</div>
+      </article>
+    </>
   );
 }
 export default Home;
