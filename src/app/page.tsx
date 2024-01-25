058e0acb8cfedcbeaee85173ad9fbe53a464cf8a
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-01-25 18:43:47 -0300

    Added styles and image to the comment section of the landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,11 @@ function Home() {
             <h1 className={styles.title}>IN/VCNITY</h1>
             <span className={styles.titleUnderline}></span>
           </div>
-          <p className={styles.text}>The place to find your place</p>
+          <p className={styles.text}>
+            Tame the Real Estate Maze. Organize, track, and conquer your search.
+            <br></br>
+            <strong>IN/VCNITY</strong> is the place to find your place!
+          </p>
           <ActionButton />
         </div>
       </article>
@@ -139,8 +143,52 @@ function Home() {
       </article>
 
       {/** --- Complementario INFO section --- */}
-      <article className={styles.contact}>
-        <div className={styles.contentContainer}>What is IN/VCNITY?</div>
+      <article className={styles.usersComments}>
+        <span className={styles.wave}>
+          <svg viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg">
+            <path
+              fill="#ff6d60"
+              fillOpacity="1"
+              d="M0,60L80,65C160,70,320,80,480,73.3C640,67,800,43,960,33.3C1120,23,1280,27,1440,26.7C1600,27,1760,23,1920,21.7C2080,20,2240,20,2400,30C2560,40,2720,60,2880,71.7C3040,83,3200,87,3360,83.3C3520,80,3680,70,3840,56.7C4000,43,4160,27,4320,31.7C4480,37,4640,63,4800,65C4960,67,5120,43,5280,43.3C5440,43,5600,67,5760,76.7C5920,87,6080,83,6240,70C6400,57,6560,33,6720,35C6880,37,7040,63,7200,70C7360,77,7520,63,7680,58.3C7840,53,8000,57,8160,53.3C8320,50,8480,40,8640,31.7C8800,23,8960,17,9120,25C9280,33,9440,57,9600,66.7C9760,77,9920,73,10080,61.7C10240,50,10400,30,10560,23.3C10720,17,10880,23,11040,31.7C11200,40,11360,50,11440,55L11520,60L11520,100L11440,100C11360,100,11200,100,11040,100C10880,100,10720,100,10560,100C10400,100,10240,100,10080,100C9920,100,9760,100,9600,100C9440,100,9280,100,9120,100C8960,100,8800,100,8640,100C8480,100,8320,100,8160,100C8000,100,7840,100,7680,100C7520,100,7360,100,7200,100C7040,100,6880,100,6720,100C6560,100,6400,100,6240,100C6080,100,5920,100,5760,100C5600,100,5440,100,5280,100C5120,100,4960,100,4800,100C4640,100,4480,100,4320,100C4160,100,4000,100,3840,100C3680,100,3520,100,3360,100C3200,100,3040,100,2880,100C2720,100,2560,100,2400,100C2240,100,2080,100,1920,100C1760,100,1600,100,1440,100C1280,100,1120,100,960,100C800,100,640,100,480,100C320,100,160,100,80,100L0,100Z"
+            ></path>
+          </svg>
+        </span>
+
+        <div className={styles.contentContainer}>
+          <div className={styles.commentsContainer}>
+            <blockquote className={styles.blockquote}>
+              <p className={styles.quote}>
+                <strong>Finally</strong> a real estate site that remembers my
+                searches and <strong>helps me stay organized!</strong>
+              </p>
+              <cite className={styles.cite}>Jorge B.</cite>
+            </blockquote>
+            <blockquote className={styles.blockquote}>
+              <p className={styles.quote}>
+                <strong>Love</strong> being able to update property info after
+                visits - <strong>makes my search so much easier!</strong>
+              </p>
+              <cite className={styles.cite}>Mónica G.</cite>
+            </blockquote>
+            <blockquote className={styles.blockquote}>
+              <p className={styles.quote}>
+                <strong>All the info I need in one place</strong> - listings,
+                notes, brochures, even market trends!
+                <strong> A lifesaver</strong>
+              </p>
+              <cite className={styles.cite}>JP B.</cite>
+            </blockquote>
+          </div>
+
+          <div className={styles.imageContainer}>
+            <Image
+              src="/images/ok-animate.svg"
+              alt="Happy customer!"
+              width={520}
+              height={10}
+            />
+          </div>
+        </div>
       </article>
     </>
   );
