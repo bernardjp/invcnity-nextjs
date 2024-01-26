6f5faf5e3c430b971017e720cb4bfb58dd8f6b84
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-01-26 18:52:19 -0300

    Refactored part of the landing page to make use of InfoSnippets and UserComments components. Added a title and a prompt to action to the user comments section of the landing page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { Metadata } from 'next';
 import Image from 'next/image';
 import styles from './style.module.css';
 import ActionButton from './components/LandingPage/ActionButton';
+import InfoSnippet from './components/LandingPage/InfoSnippet';
+import UserComment from './components/LandingPage/UserComment';
 
 export const metadata: Metadata = {
   title: 'IN/VCNITY',
@@ -11,7 +13,7 @@ export const metadata: Metadata = {
 function Home() {
   return (
     <>
-      <article className={styles.hero}>
+      <section className={styles.hero}>
         <div className={styles.imageContainer}>
           <Image
             src="/images/select-house-couple.svg"
@@ -33,117 +35,69 @@ function Home() {
           </p>
           <ActionButton />
         </div>
-      </article>
-      <article className={styles.about}>
+      </section>
+      <section className={styles.about}>
         <div className={styles.contentContainer}>
           {/** ----- First item ----- */}
-          <div className={styles.itemWrapper} style={{ alignItems: 'center' }}>
-            <div className={styles.imageContainer}>
-              <Image
-                src="/images/for-sale-cropped.svg"
-                alt="Real Estate on Sale"
-                width={520}
-                height={10}
-              />
-            </div>
-            <div className={styles.textWrapperRight}>
-              <div className={styles.textRightContainer}>
-                <div className={styles.titleContainer}>
-                  <h3 className={styles.secondaryTitle}>
-                    Store your Real Estate findings
-                  </h3>
-                  <span className={styles.secondaryTitleUnderline}></span>
-                </div>
-                <p className={styles.text}>
-                  Ever come across that perfect property, only to forget the
-                  details later? With our website, you can save your searches,
-                  favorite listings, and property notes all in one secure place.
-                  No more lost gems or time wasted retracing your steps. Find
-                  your dream home faster with your personalized real estate
-                  portfolio at your fingertips.
-                </p>
-              </div>
-            </div>
-          </div>
+          <InfoSnippet
+            image={{
+              url: '/images/for-sale-cropped.svg',
+              alt: 'Real Estate on Sale',
+            }}
+            first={true}
+            side="Left"
+            title="Store your Real Estate findings and build your own portfolio"
+          >
+            Ever come across that perfect property, only to forget the details
+            later? With our website, you can save your searches, favorite
+            listings, and property notes all in one secure place.{' '}
+            <strong>
+              No more lost gems or time wasted retracing your steps.
+            </strong>{' '}
+            Find your dream home faster with your personalized real estate
+            portfolio at your fingertips.
+          </InfoSnippet>
 
           {/** ----- Second item ----- */}
-          <div className={styles.itemWrapper}>
-            <div className={styles.textWrapperLeft}>
-              {/** --- Arrow --- */}
-              <span className={styles.arrowLeft}>
-                <span className={styles.topLeftPoint}></span>
-                <span className={styles.bottomRightPoint}></span>
-              </span>
-
-              <div className={styles.textLeftContainer}>
-                <div className={styles.titleContainer}>
-                  <h3 className={styles.secondaryTitle}>
-                    Paid them a visit and update your data
-                  </h3>
-                  <span className={styles.secondaryTitleUnderline}></span>
-                </div>
-                <p className={styles.text}>
-                  Don&apos;t let outdated information hold you back. After
-                  touring a property, quickly mark it as &quot;visited&quot; on
-                  our platform. This helps you keep track of your progress and
-                  avoid revisiting the same places. Update notes, add
-                  impressions, and rate your experience to personalize your
-                  search journey and make informed decisions.
-                </p>
-              </div>
-            </div>
-
-            <div className={styles.imageContainer}>
-              <Image
-                src="/images/realtor-visit.svg"
-                alt="Couple visiting a Real Estate on Sale"
-                width={520}
-                height={10}
-              />
-            </div>
-          </div>
+          <InfoSnippet
+            image={{
+              url: '/images/realtor-visit.svg',
+              alt: 'Couple visiting a Real Estate on Sale',
+            }}
+            side="Right"
+            title="Paid them a visit and update your data"
+          >
+            Don&apos;t let outdated information hold you back. After touring a
+            property, quickly mark it as &quot;visited&quot; on our platform.
+            This helps you keep track of your progress and avoid revisiting the
+            same places.{' '}
+            <strong>
+              Update notes, add impressions, and rate your experience
+            </strong>{' '}
+            to personalize your search journey and make informed decisions.
+          </InfoSnippet>
 
           {/** ----- Third item ----- */}
-          <div className={styles.itemWrapper}>
-            <div className={styles.imageContainer}>
-              <Image
-                src="/images/house-at-hand.svg"
-                alt=""
-                width={520}
-                height={10}
-              />
-            </div>
-            <div className={styles.textWrapperRight}>
-              {/** --- Arrow --- */}
-              <span className={styles.arrowRight}>
-                <span className={styles.topRightPoint}></span>
-                <span className={styles.bottomLeftPoint}></span>
-              </span>
-
-              {/** --- Text Content --- */}
-              <div className={styles.textRightContainer}>
-                <div className={styles.titleContainer}>
-                  <h3 className={styles.secondaryTitle}>
-                    Keep all the relevant information close at hand
-                  </h3>
-                  <span className={styles.secondaryTitleUnderline}></span>
-                </div>
-                <p className={styles.text}>
-                  Stay organized and informed throughout your real estate
-                  journey. Our platform stores property brochures, agent contact
-                  details, market trends, and any other relevant information
-                  alongside your saved listings. No more digging through emails
-                  or scattered notes. Access everything you need, whenever you
-                  need it, for a seamless and stress-free experience.
-                </p>
-              </div>
-            </div>
-          </div>
+          <InfoSnippet
+            image={{
+              url: '/images/house-at-hand.svg',
+              alt: 'Hand holding a house',
+            }}
+            side="Left"
+            title="Keep all the relevant information close at hand"
+          >
+            Stay organized and informed throughout your real estate journey. Our
+            platform stores property brochures, agent contact details, market
+            trends, and any other relevant information alongside your saved
+            listings. No more digging through emails or scattered notes.{' '}
+            <strong>Access everything you need, whenever you need it</strong>,
+            for a seamless and stress-free experience.
+          </InfoSnippet>
         </div>
-      </article>
+      </section>
 
       {/** --- Complementario INFO section --- */}
-      <article className={styles.usersComments}>
+      <section className={styles.usersComments}>
         <span className={styles.wave}>
           <svg viewBox="0 0 1440 100" xmlns="http://www.w3.org/2000/svg">
             <path
@@ -154,42 +108,51 @@ function Home() {
           </svg>
         </span>
 
-        <div className={styles.contentContainer}>
-          <div className={styles.commentsContainer}>
-            <blockquote className={styles.blockquote}>
-              <p className={styles.quote}>
+        <article className={styles.contentContainer}>
+          <div className={styles.titleWrapper}>
+            <h3 className={styles.commentsTitle}>
+              <strong>Sharing is caring. </strong>This is what our users say
+              about IN/VCNITY!
+            </h3>
+          </div>
+
+          <div className={styles.commentSectionWrapper}>
+            <div className={styles.commentsContainer}>
+              <UserComment citeText="Jorge B.">
                 <strong>Finally</strong> a real estate site that remembers my
                 searches and <strong>helps me stay organized!</strong>
-              </p>
-              <cite className={styles.cite}>Jorge B.</cite>
-            </blockquote>
-            <blockquote className={styles.blockquote}>
-              <p className={styles.quote}>
+              </UserComment>
+              <UserComment citeText="Mónica G.">
                 <strong>Love</strong> being able to update property info after
                 visits - <strong>makes my search so much easier!</strong>
-              </p>
-              <cite className={styles.cite}>Mónica G.</cite>
-            </blockquote>
-            <blockquote className={styles.blockquote}>
-              <p className={styles.quote}>
+              </UserComment>
+              <UserComment citeText="JP B.">
                 <strong>All the info I need in one place</strong> - listings,
                 notes, brochures, even market trends!
                 <strong> A lifesaver</strong>
-              </p>
-              <cite className={styles.cite}>JP B.</cite>
-            </blockquote>
+              </UserComment>
+            </div>
+
+            <div className={styles.imageContainer}>
+              <Image
+                src="/images/ok-animate.svg"
+                alt="Happy customer!"
+                width={520}
+                height={10}
+              />
+            </div>
           </div>
 
-          <div className={styles.imageContainer}>
-            <Image
-              src="/images/ok-animate.svg"
-              alt="Happy customer!"
-              width={520}
-              height={10}
-            />
+          <div className={styles.titleWrapper}>
+            <span className={styles.titleUnderline}></span>
+            <h3 className={styles.commentsTitle}>
+              Join <strong>IN/VCNITY</strong> now and let us help you find your
+              dream home faster than ever!
+            </h3>
+            <ActionButton />
           </div>
-        </div>
-      </article>
+        </article>
+      </section>
     </>
   );
 }
