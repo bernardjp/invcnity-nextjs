a8f9f9718906eaed5c4e3a6f4355b9be539dfdb8
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-01-29 16:20:05 -0300

    Added NextJS error handlers to each page tied to a Firebase service.

---
+++ src/app/propiedades/[id]/error.tsx
@@ -0,0 +1,14 @@
+'use client';
+import ErrorHandling from '../../components/ErrorHandling';
+
+type ErrorProps = {
+  error: Error & { digest?: string };
+  reset: () => void;
+};
+
+function Error(props: ErrorProps) {
+  const { error, reset } = props;
+  return <ErrorHandling actionCallback={() => reset()} error={error} />;
+}
+
+export default Error;
