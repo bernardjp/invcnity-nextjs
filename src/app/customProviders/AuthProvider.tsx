26820a06847a222a4d3676fb1f05ae517070e4fe
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-02-01 14:48:45 -0300

    Added a custom HoC that handles redirections after the user logs off the app.

---
+++ src/app/customProviders/AuthProvider.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+import { auth } from '@/firebase/clientApp';
+import { useRouter } from 'next/navigation';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) router.replace('/');
+  }, [user, router]);
+
+  return <>{children}</>;
+}
+
+export default AuthProvider;
