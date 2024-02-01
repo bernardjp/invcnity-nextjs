7405fe67ab2f69933c2f10e28e8dc44d0b17947b
Juan Pablo Bernard <56048120+bernardjp@users.noreply.github.com>
2024-02-01 11:19:24 -0300

    Added disabled behavior to the Menu's items.

--- src/app/components/DashboardHandler/EstateTitleMenu.tsx
+++ src/app/components/DashboardHandler/EstateTitleMenu.tsx
@@ -76,6 +76,7 @@ function EstateTitleMenu(props: Props) {
       onClickHandler: onEditHandler,
     },
     {
+      disabled: true,
       icon: <ExternalLinkIcon />,
       text: 'Share Estate',
       onClickHandler: onShareHandler,
@@ -92,6 +93,7 @@ function EstateTitleMenu(props: Props) {
       {itemsData.map((item: any) => (
         <TitleMenuItem
           key={item.text}
+          disabled={item.disabled}
           icon={item.icon}
           style={styles.item}
           text={item.text}
